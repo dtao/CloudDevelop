9963368539b940c704105c2adbaa49389a385d9f
dtao <dtao@thoughtworks.com>
2011-03-29 01:20:59 -0500

    initial commit of CloudDevelop project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudDevelop::Application.config.secret_token = 'f5751a65c04c8a91126ed6e0bac19a2db9391dee53df1e07201a75bcaee944ba747959f9a1ae96332f14118c29f6312acd409c7fac39fac7c7cdddad41d13619'
