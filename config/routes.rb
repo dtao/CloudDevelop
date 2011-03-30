0b3437ae377063d61ea984a099d6c1a4f31540ef
dtao <dtao@thoughtworks.com>
2011-03-29 21:38:52 -0500

    updated header (made nav bar less unnecessarily huge) and added Project model w/ controller & view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 CloudDevelop::Application.routes.draw do
+  resources :projects
+
   resources :tasks
 
   # The priority is based upon order of creation:
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110329024518) do
+ActiveRecord::Schema.define(:version => 20110330023546) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", :force => true do |t|
     t.string   "description"
