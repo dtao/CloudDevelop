6e21dc185138fb2e40702d56419cbe14990778a2
Dan <daniel.tao@jpmorgan.com>
2011-07-09 19:02:55 -0700

    reorganized files, implemented VERY basic ajax interaction between view and CodeSnippetsController

---
+++ public/javascripts/service.js
@@ -0,0 +1,53 @@
+ideoneService = (function() {
+	function createPromise() {
+		var successHandler = function() {},
+			errorHandler = function() {};
+		
+		function onSuccess(handler) {
+			successHandler = handler;
+			return this;
+		}
+
+		function onError(handler) {
+			errorHandler = handler;
+			return this;
+		}
+
+		function success(data) {
+			successHandler(data);
+		}
+
+		function error(data) {
+			errorHandler(data);
+		}
+		
+		return {
+			onSuccess: onSuccess,
+			onError: onError,
+			success: success,
+			error: error
+		};
+	}
+
+	function execute(code) {
+		var promise = createPromise();
+
+		$.ajax("/code_snippets.js", {
+			data: { code_snippet: code },
+			dataType: 'json',
+			type: 'post',
+			success: function(data, textStatus, jqXHR) {
+				promise.success(data);
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				promise.error(textStatus);
+			}
+		});
+
+		return promise;
+	}
+
+	return {
+		execute: execute
+	};
+}());
