aff606ab29ff29fc17ee41bf25a50cd310524329
Dan Tao <daniel.tao@gmail.com>
2012-10-22 20:46:37 -0700

    added authentication through Google

--- app/app.rb
+++ app/app.rb
@@ -13,9 +13,11 @@ configure do
   require File.join(PROJECT_ROOT, "config", "boot")
   require "omniauth"
   require "omniauth-github"
+  require "omniauth-google-oauth2"
 
   use OmniAuth::Builder do
     provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"], :scope => "gist"
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], { :access_type => "online", :approval_prompt => "" }
   end
 
   Engine.register(:haml, HAMLEngine.new)
